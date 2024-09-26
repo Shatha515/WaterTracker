718283e71e3535671e721ed4eab6fc4bd04217de
Shatha515 <alshareefshatha34@gmail.com>
2024-09-26 12:53:06 +0300

    Initial Commit

---
+++ WaterTracker/WaterTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterTrackerApp.swift
+//  WaterTracker
+//
+//  Created by Shatha Alsharif on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct WaterTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
